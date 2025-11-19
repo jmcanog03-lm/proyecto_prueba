7deb3c00dc1bf4863af0ee64fe4f6212e5d710a2
jmcanog03-lm <jmcanog03@iesalbarregas.es>
2025-11-19 18:27:30 +0100

    añadido función que saca un mensaje por pantalla

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+    }
+
+    public static void funcion_syso(){
+        System.out.println("Esto es una función");
     }
 }
